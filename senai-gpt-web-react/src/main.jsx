f37aca4d6e8ca2879287b406fd7cfc8fea2dbab1
Kaue <kauebarbosa235@gmail.com>
2025-04-16 21:51:28 -0300

    redeslogin

--- senai-gpt-web-react/src/main.jsx
+++ senai-gpt-web-react/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import "./assets/styles/global.css"
+import Login from './pages/login/index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Login />
   </StrictMode>,
 )
--- senai-gpt-web-react/src/pages/login/index.jsx
+++ senai-gpt-web-react/src/pages/login/index.jsx
@@ -1,6 +1,37 @@
 import "./login.css"
+import logo from "../../assets/imgs/Chat (1).svg"
+import { useState } from "react";
 
 function Login() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onLoginClick = async () => {
+
+   let response = await fetch ("https://senai-gpt-api.azurewebsites.net/login", {
+
+      headers: {
+
+       "Content-Type": "application/json" 
+
+      },
+
+      method: "POST", //metodo que envia dados
+      body: JSON.stringify ({
+
+    email: email,
+    password: password
+
+      }) 
+
+   });
+
+   console.log(response);
+
+
+
+  }
 
   return (
     <>
@@ -11,18 +42,18 @@ function Login() {
 
         <div className="login-container">
 
-          <img className="logo" src="../assets/imgs/Chat.png" alt="Logo do SenaiGPT." />
+          <img className="logo" src={logo} alt="Logo do SenaiGPT." />
 
           <h1
-            
+
             id="meutitulo"
             className="titulo"
           >Login</h1>
 
-          <input className="inpt" type="email" placeholder="Insira o e-mail" />
-          <input className="inpt" type="password" placeholder="Insira a senha" />
+          <input className="inpt" value={email} onChange={event => setEmail(event.target.value)} type="email" placeholder="Insira o e-mail" />
+          <input className="inpt" value={password} onChange={event => setPassword(event.target.value)} type="password" placeholder="Insira a senha" />
 
-          <button className="btn">Entrar</button>
+          <button className="btn" onClick={() => onLoginClick()} >Entrar</button>
 
         </div>
 
